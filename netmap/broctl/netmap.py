64c41732eccad2bc15119c909d5d29433e5041ab
Seth Hall <seth@icir.org>
2016-09-02 11:09:01 -0400

    Fix up the broctl netmap plugin.
    It didn't do things right if you were not sniffing a pipe
    (i.e., not load balancing) or if you were sniffing two
    separate load balanced sets of pipes from two interfaces
    on a single system. These are now handled correctly.

--- netmap/broctl/netmap.py
+++ netmap/broctl/netmap.py
@@ -1,4 +1,3 @@
-
 import BroControl.plugin
 from BroControl import config
 
@@ -15,18 +14,23 @@ class Netmap(BroControl.plugin.Plugin):
     def init(self):
         useplugin = False
 
-        i=0
+        interface_pipes={}
         for nn in self.nodes():
-            if nn.type != "worker" or not nn.interface.startswith("netmap::"):
+            # Only do this if it's a worker, starts with netmap:: and has some lb_proc configured.
+            if nn.type != "worker" or not nn.interface.startswith("netmap::") or nn.lb_procs == "":
                 continue
 
             useplugin = True
 
-            if i == 0 and nn.netmap_first_pipe:
-                i = int(nn.netmap_first_pipe)
+            orig_if = nn.interface
+            if (orig_if,nn.host) not in interface_pipes:
+                i=0
+                if nn.netmap_first_pipe:
+                    i = int(nn.netmap_first_pipe)
+                interface_pipes[nn.host,orig_if] = i
 
-            nn.interface="{:s}}}{:d}".format(nn.interface, i)
-            i = i+1
+            nn.interface="{:s}}}{:d}".format(orig_if, interface_pipes[nn.host,orig_if])
+            interface_pipes[nn.host,orig_if] = interface_pipes[nn.host,orig_if]+1
 
         return useplugin
 
@@ -35,4 +39,3 @@ class Netmap(BroControl.plugin.Plugin):
 
     def broctl_config(self):
         pass
-
